df95764bef7c8f3fa322e87898bd5188f7653656
karthick <karthickmv14@gmail.com>
2026-08-29 20:29:36 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
 
+          
         </Routes>
       </BrowserRouter>
     </>
